3024a914c7078d571b0115b626f2a32ba33f5a52
cuebitt <136763482+cuebitt@users.noreply.github.com>
2024-10-07 01:36:04 -0700

    Remove blog, greenhouse, about -> homepage

--- site/homepage/script.js
+++ site/homepage/script.js
@@ -1,3 +1,10 @@
+import confetti from "https://esm.sh/canvas-confetti@1.6.0";
+
+// Set age value
+document.getElementById("age-text").textContent =
+  `${Math.floor(Math.abs(Date.now() - new Date("2001-05-21")) / 31556952000)}yrs`;
+
+// Set pronouns from PronounDB
 const pronounDbId = `0185dc21-e518-736c-aa79-6f1e365f064e`;
 fetch(`https://pronoundb.org/api/v2/users/${pronounDbId}`)
   .then((res) => res.json())
@@ -46,5 +53,46 @@ fetch(`https://pronoundb.org/api/v2/users/${pronounDbId}`)
     }
 
     const pText = document.getElementById("pronouns-text");
+    pText.removeAttribute("aria-busy");
+    pText.dataset.tooltip = "via pronoundb.org";
     pText.textContent = pStr;
   });
+
+// Add event handlers to dialog buttons
+const nsoModal = document.getElementById("nso-modal");
+document.getElementById("nso-btn").addEventListener("click", () => {
+  nsoModal.showModal();
+});
+document
+  .querySelector("#nso-modal > article > header > button[rel=prev]")
+  .addEventListener("click", () => {
+    nsoModal.close();
+  });
+
+const discordModal = document.getElementById("discord-modal");
+document.getElementById("discord-btn").addEventListener("click", () => {
+  discordModal.showModal();
+});
+document
+  .querySelector("#discord-modal > article > header > button[rel=prev]")
+  .addEventListener("click", () => {
+    discordModal.close();
+  });
+
+// Add event handler (confetti) to the profile photo
+document.querySelector(".avatar-container").addEventListener("click", (e) => {
+  const box = e.target.getBoundingClientRect();
+  const docElem = document.documentElement;
+
+  const origin = {
+    x:
+      (box.left + window.scrollX - docElem.clientLeft + box.width / 2) /
+      window.innerWidth,
+    y:
+      (box.top + window.scrollY - docElem.clientTop + box.height) /
+      window.innerHeight,
+  };
+  confetti({
+    origin,
+  });
+});
