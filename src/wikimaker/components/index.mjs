fd6cfe0a794b5bce410fe1fb704c9321a0bee27b
cuebitt <136763482+cuebitt@users.noreply.github.com>
2024-10-23 19:50:02 -0700

    wikimaker-eta -> wikimaker

--- src/wikimaker/components/index.mjs
+++
@@ -1,6 +0,0 @@
-import { Toc } from "./toc.mjs";
-import { Header } from "./header.mjs";
-import { RightCard } from "./rightCard.mjs";
-import { MainContent } from "./mainContent.mjs";
-
-export { Toc, Header, RightCard, MainContent };
